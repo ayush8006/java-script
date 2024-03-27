4e8dee567d4e47e38b1f07b95a68895a2074a930
ayush8006 <ayushagrawal8006@gmail.com>
2024-03-27 17:54:31 +0530

    DOM

--- module_6/coercion/default.js
+++ module_6/coercion/default.js
@@ -1,8 +1,8 @@
 //console.log("njasm")
 
-console.log(1-7)
-console.log(2-null)
-console.log(2-undefined)
+// console.log(1-7)
+// console.log(2-null)
+// console.log(2-undefined)
 console.log(2-"838")
 console.log(2-"jhkqdj92w")
 console.log(this);
